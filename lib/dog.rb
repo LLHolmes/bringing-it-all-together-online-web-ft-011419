38136137a576d067c9eae27f9d703af22e85fe05
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-15 14:26:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,7 +49,7 @@ class Dog
     if dog_info.empty?
       dog = Dog.create(name: name, breed: breed)
     else
-      dog = Dog.find_by_name(name)
+      dog = Dog.find_by_id(dog_info[0])
   end
   
   def self.create_table
