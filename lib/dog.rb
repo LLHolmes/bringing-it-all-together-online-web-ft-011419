da7a74a1aa4e157f5c9a2ae48d3f9210e306b8dc
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-15 13:55:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,8 +25,8 @@ class Dog
   end
   
   def self.find_by_name(name)
-    dog_info = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name)[0]
-    self.new_from_db(dog_info)
+    dog_info = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name)
+    self.new_from_db(dog_info[0])
   end
   
   def self.find_or_create_by(name)
