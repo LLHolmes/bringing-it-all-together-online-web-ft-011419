35552054611bec1a941a798b67ca6d791cf1c114
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-15 13:49:56 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,47 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+  
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+  
+  def save
+  end
+  
+  def update
+  end
+  
+  def self.create(row)
+  end
+  
+  def self.new_from_db(row)
+  end
+  
+  def self.find_by_id(id)
+  end
+  
+  def self.find_by_name(name)
+  end
+  
+  def self.find_or_create_by(name)
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE dogs")
+  end
+  
+end
